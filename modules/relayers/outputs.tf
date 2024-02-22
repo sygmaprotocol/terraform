5dc72a0a95974529ce72394d2baa6df925710731
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2024-02-22 14:48:04 +0100

    refactored outputs

--- modules/relayers/outputs.tf
+++ modules/relayers/outputs.tf
@@ -1,26 +1,13 @@
-locals {
-  lb_names = tomap({ for i in var.relayers_name : i => null })
+data "aws_lb" "dns" {
+  for_each = toset(var.relayers_name)
+  name = "${var.project_name}-${each.value}-lb-${var.env_sufix}"
 }
 
-resource "local_file" "dns" {
-  for_each = local.lb_names
-  content  = aws_lb.main[each.key].dns_name
-  filename = "${path.module}/dns/dns.${each.key}"
-}
-
-resource "null_resource" "dns" {
-  triggers = {
-    always_run = "${timestamp()}"
+output "lb_names_arns" {
+  value = {
+    for lb in data.aws_lb.dns : lb.id => {
+      name = lb.name
+      arn  = lb.arn
+    }
   }
-  provisioner "local-exec" {
-    working_dir = "${path.module}/dns/"
-    command     = <<EOF
-      rm dns_address
-      for i in dns.*; do (cat $i; echo '') >> dns_address; done
-      rm dns.*
-    EOF
-  }
-  depends_on = [
-    local_file.dns
-  ]
 }
