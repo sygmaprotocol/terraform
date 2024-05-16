38328e513f79947bbd9b92bc64056d6613d91b6e
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2024-05-16 15:40:24 +0100

    fixed: dynamic changes in Availability zones

--- modules/relayers/efs.tf
+++ modules/relayers/efs.tf
@@ -12,10 +12,10 @@ resource "aws_efs_file_system" "efs" {
 }
 
 resource "aws_efs_mount_target" "main" {
-  count = length(data.aws_subnets.ec2_private_subnets.ids)
+  for_each = toset(data.aws_subnets.ec2_private_subnets.ids)
 
   file_system_id = aws_efs_file_system.efs.id
-  subnet_id      = element(data.aws_subnets.ec2_private_subnets.ids, count.index)
+  subnet_id      = "${each.key}"
 
   security_groups = [
     aws_security_group.efs.id,
@@ -25,11 +25,11 @@ resource "aws_efs_mount_target" "main" {
 
 resource "aws_security_group" "efs" {
   name        = "${var.project_name}-efs-${var.env_sufix}"
-  description = "relayer for efs volume in Devnet environment"
+  description = "efs volume in ${var.env_sufix}"
   vpc_id      = data.aws_vpc.vpc.id
 
   ingress {
-    description = "Allow EFS to connect to the STAGE VPC"
+    description = "Allow EFS to connect to the ${var.env_sufix} VPC"
     from_port   = var.efs_port
     to_port     = var.efs_port
     protocol    = "tcp"
